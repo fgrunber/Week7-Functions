f219357274ebf9e2fb84aac45c5aa43d70f0a316
fgrunber <96977461+fgrunber@users.noreply.github.com>
2022-01-18 16:23:33 +0200

    add new projects

---
+++ UserNameData/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UserNameData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programm küsib kasutajal sisestada oma eesnime
+            //Programm kuvab kasutaja eesnime pikkuse
+            //Programm kuvab kasutaja eesnime esimese tähe
+            //Programm kuvab kasutaja eesnime tagurpidi
+            //Main meetodis ei tohi olla rohkem, kui kolm rida koodi
+
+            Console.WriteLine("Sisesta oma eesnimi");
+            string userName = Console.ReadLine();
+            GetUserNameData(userName);
+        }
+
+        public static void GetUserNameData(string userInput)
+        {
+            Console.WriteLine($"Sinu nimi on {userInput.Length} sumblit pikk.");
+            Console.WriteLine($"Sinu eesnimi esimene taht on {userInput[0]}");
+
+            for(int i = userInput.Length - 1; i>= 0; i++)
+            {
+                Console.Write(userInput[i]);
+            }
+        }
+    }
+}
